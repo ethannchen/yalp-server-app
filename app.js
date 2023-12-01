ece89e58011c2e058c399c6af2379d2f74d37acf
yy <yichen@YY-MacBook-Air.local>
2023-11-30 19:05:52 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import session from "express-session";
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/yalp";
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+app.listen(process.env.PORT || 4000);

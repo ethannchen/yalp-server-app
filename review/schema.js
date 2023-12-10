0339dbc336d5e5fea8328e5b83955da1798c2bdf
Zhiyan <pei.zhiy@northeastern.edu>
2023-12-09 18:14:15 -0800

    submit 12/9

--- review/schema.js
+++ review/schema.js
@@ -4,7 +4,7 @@ const reviewSchema = new mongoose.Schema(
     // user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
     userId: { type: String, required: true },
     restaurantId: { type: String, required: true },
-    content: String,
+    content: { type: String, required: true },
     // date: Date,
     // img: URL,
   },

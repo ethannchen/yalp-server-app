bf11a31dd741bd4c80b3a0706fa6723abc707cb1
xialingfeng <lingfengxia.jake@gmail.com>
2023-12-10 11:04:23 -0500

    add routes

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import session from "express-session";
-
+import userRoute from "./users/routes.js"
+import reviewRoute from "./review/routes.js"
+import homepageRoute from "./homepage/routes.js"
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/yalp";
 mongoose.connect(CONNECTION_STRING);
@@ -32,6 +34,9 @@ if (process.env.NODE_ENV !== "development") {
     secure: true,
   };
 }
+userRoute(app);
+reviewRoute(app);
+homepageRoute(app);
 app.use(session(sessionOptions));
 
 app.listen(process.env.PORT || 4000);
